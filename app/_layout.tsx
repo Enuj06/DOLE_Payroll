26560733e8a5f61a295aee02995b610b40124597
MosesAlcantara <mosesalcantara123@gmail.com>
2025-11-06 13:17:29 +0800

    Add loader and wrap root layout in suspense

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,16 @@
+import Loader from "@/components/Loader";
 import "@/globals.css";
 import { Stack } from "expo-router";
+import { Suspense } from "react";
 import "react-native-reanimated";
 
 const RootLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <Suspense fallback={<Loader />}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </Suspense>
   );
 };
 
